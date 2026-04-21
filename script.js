89a41f555344029c78208b8fef7e1a2994b9da50
firemanthinks <firemanthinks.com>
2026-04-21 14:13:28 +0500

    Abdiyozov Shahzodbek's portfoliyo

--- script.js
+++ script.js
@@ -152,3 +152,11 @@ modal.addEventListener("click", (e) => {
         modal.classList.remove("active");
     }
 });
+
+
+const hamburger = document.getElementById("hamburger");
+const navbar = document.querySelector(".navbar");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+}); 
